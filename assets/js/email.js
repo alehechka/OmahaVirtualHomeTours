b15fd1ac3110e5b1451ba455b68589b4390d89bc
alehechka <ajlehechka@gmail.com>
2020-08-15 17:46:40 -0500

    Refactor email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,7 +1,13 @@
+const adamEmail = 'ajlehechka@gmail.com';
+const ryanEmail = 'ryan.a.bahn@gmail.com';
+
+const toEmails = [adamEmail, ryanEmail];
+
+const mailto = toEmails.reduce((acc, email) => `${acc}; ${email}`, '')
+
 function send() {
-    console.log('hello')
     setTimeout(function() {
-      window.open("mailto:" + 'ajlehechka@gmail.com; ryan.a.bahn@gmail.com' 
+      window.open("mailto:" + mailto 
       + "?subject=" + document.getElementById('subject').value 
       + "&body=" + "Hi, this is " + document.getElementById('name').value + ". \n" + document.getElementById('message').value);
     }, 320);
